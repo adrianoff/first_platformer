ba4bed3cef5012394f8c4337c7cf715b3c9a60b4
adrianoff <adrianov87@gmail.com>
2022-07-10 22:55:18 +0300

    enemy. sound.

--- Assets/Scripts/Coin.cs
+++ Assets/Scripts/Coin.cs
@@ -2,13 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Saw : MonoBehaviour
+public class Coin : MonoBehaviour
 {
+    public AudioSource collectSound;
+
     public void OnCollisionEnter2D(Collision2D other) 
     {
         if(other.gameObject.tag == "Player") {
-            other.gameObject.GetComponent<Rigidbody2D>().AddForce(transform.up * 8f, ForceMode2D.Impulse);
-            other.gameObject.GetComponent<Player>().RecountHP();
+            collectSound.Play();
         }
+
+        this.gameObject.SetActive(false);
     }
 }
